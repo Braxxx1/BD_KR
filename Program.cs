9fc3505999ad857e759dace925c25ff791780130
Braxxx1 <50825640+Braxxx1@users.noreply.github.com>
2023-12-18 12:11:48 +0300

    Serch words

--- Program.cs
+++ Program.cs
@@ -7,3 +7,19 @@ foreach (var item in wordSpis)
         size += 1;
     }    
 }
+
+string[] newWordSpis = new string[size];
+int index = 0;
+foreach (var item in wordSpis)
+{
+    if (item.Length <= 3)
+    {
+        newWordSpis[index] = item;
+        index++;
+    }    
+}
+
+foreach (var item in newWordSpis)
+{
+    Console.WriteLine(item);
+}
